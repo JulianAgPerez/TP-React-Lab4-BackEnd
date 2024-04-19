4ed6bbab0d6480f78d41cae131a57f6c6a076416
JulianAgPerez <julian.perez@alumnos.frm.utn.edu.ar>
2024-04-19 12:38:50 -0300

    firts commit

---
+++ src/main/java/com/utn/tpreactbackend/TpReactBackendApplication.java
@@ -0,0 +1,14 @@
+package com.utn.tpreactbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpReactBackendApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TpReactBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/utn/tpreactbackend/TpReactBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.tpreactbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpReactBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
